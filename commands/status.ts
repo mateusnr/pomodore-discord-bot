10f32e00f69aaafa018037a7210eed7e430d47d6
Mateus Nóbrega <mateus@nobrega.dev>
2021-11-14 16:41:30 -0300

    Port the status command

---
+++ commands/status.ts
@@ -0,0 +1,39 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Command, CommandOptions } from "@sapphire/framework";
+import type { Message } from "discord.js";
+import PomodoroContainer from "../container";
+
+@ApplyOptions<CommandOptions>({
+    name: 'status',
+    description: `Check a pomodoro's current status`
+})
+export class PomodoroStatusCommand extends Command {
+    public async messageRun(message: Message) {
+        const container = PomodoroContainer.getInstance();
+        let pomodoro = container.pomodoros.filter(
+            (pomodoro) => pomodoro.guild.id == message.guild!.id
+        );
+
+        if (pomodoro.length == 0) {
+            message.channel.send("There's no pomodoro currently running!");
+            return;
+        }
+
+        let now = new Date();
+        let timePassed = now.getTime() - pomodoro[0].timerStartedTime.getTime();
+        let timeLeft;
+
+        if (pomodoro[0].time % 2 != 0) {
+            timeLeft = Math.floor((pomodoro[0].workTime - timePassed) / 60000);
+            message.channel.send(
+                `${timeLeft + 1}min left to your break! Keep it up!`
+            );
+        } else if (pomodoro[0].time % 2 == 0 && pomodoro[0].time != 8) {
+            timeLeft = Math.floor((pomodoro[0].smallBreak - timePassed) / 60000);
+            message.channel.send(`${timeLeft + 1}min left to start working!`);
+        } else {
+            timeLeft = Math.floor((pomodoro[0].bigBreak - timePassed) / 60000);
+            message.channel.send(`${timeLeft + 1}min left to start working!`);
+        }
+    }
+}
